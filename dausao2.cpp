9ba3b42e264a4aeaff50410d0ac4faa056fe939e
Tam1032 <tamnm1032@gmail.com>
2020-12-30 21:15:00 +0700

    add void function

--- dausao2.cpp
+++ dausao2.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
-int main(){
-	for (int i=5;i>=0;i--){
-		for (int j=1;j<=5-i;j++){
-			cout<<" ";
-		}
-		for (int k=1;k<=i;k++){
-			cout<<"*";
-		}
-		cout<<endl;
+void dausao(){}
+for (int i=5;i>=0;i--){
+	for (int j=1;j<=5-i;j++){
+		cout<<" ";
 	}
+	for (int k=1;k<=i;k++){
+		cout<<"*";
+	}
+	cout<<endl;
+}}
+int main(){
+	dausao;
 	return 0;
 }
